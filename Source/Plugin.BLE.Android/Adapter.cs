fdb963107a11009048841fc41e16761706921d14
EddPrilipa <73933147+eRRp101@users.noreply.github.com>
2022-09-22 12:10:47 +0200

    Revert "BLE_error_AndroidPlugin"
    This reverts commit f37d63e04a0861525fca4f3383fa1e2b49eceddc.

--- Source/Plugin.BLE.Android/Adapter.cs
+++ Source/Plugin.BLE.Android/Adapter.cs
@@ -13,10 +13,6 @@ using Plugin.BLE.Extensions;
 using Object = Java.Lang.Object;
 using Trace = Plugin.BLE.Abstractions.Trace;
 using Plugin.BLE.Abstractions.EventArgs;
-using BC.Mobile.Managers;
-using BC.Mobile.Utilities.Messaging;
-using Unity;
-using BC.Mobile.Strings;
 
 namespace Plugin.BLE.Android
 {
@@ -291,23 +287,6 @@ namespace Plugin.BLE.Android
             {
                 Trace.Message("Adapter: Scan failed with code {0}", errorCode);
                 base.OnScanFailed(errorCode);
-
-                //Same errorcode as shown on LightBlue app ("SCAN_FAILED_APPLICATION_REGISTRATION_FAILED")
-                //Will trigger if BLE scanner fails to register app
-                //Prompt user to restart device
-                if (errorCode == (ScanFailure)2)
-                {
-                    var notificationService = BC.Mobile.AppContext.Container.Resolve<INotificationManager>();
-
-                    string errorMessage = FeatureResources.GeneralBluetooth + " " + FeatureResources.DiagnosticsError + ". " +
-                        FeatureResources.GeneralRestart + " " + FeatureResources.GeneralDevice + ".";
-
-                    notificationService.ShowNotification(new NotificationMessage(
-                        errorMessage,
-                        BC.Mobile.Utilities.Messaging.Snackbar.NotificationType.Alert,
-                        CooldownDuration.TenSecondsInSec,
-                        NotificationDuration.FifteenSecondsInMs));
-                }
             }
 
             public override void OnScanResult(ScanCallbackType callbackType, ScanResult result)
