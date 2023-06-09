638a2f3f3d8715b63630b1cfc8db8e69759bea50
John Hjerrild Sørensen <johs@bang-olufsen.dk>
2023-06-09 09:35:03 +0200

    fix-for-android

--- Source/Plugin.BLE/Android/DefaultTrace.cs
+++ Source/Plugin.BLE/Android/DefaultTrace.cs
@@ -1,40 +1,26 @@
-﻿using System;
-using BC.Mobile.Logging;
-using Plugin.BLE.Abstractions;
+﻿//using System;
+//using BC.Mobile.Logging;
+//using Plugin.BLE.Abstractions;
 
-namespace Plugin.BLE
-{
-    static class DefaultTrace
-    {
-        private static readonly ILogger _logger = LoggerFactory.CreateLogger("Plugin.BLE");
+//namespace Plugin.BLE
+//{
+//    static class DefaultTrace
+//    {
+//        private static readonly ILogger _logger = LoggerFactory.CreateLogger("Plugin.BLE");
 
-        public static void DefaultTraceInit()
-        {
-            Trace.TraceImplementation = (parameter, args) =>
-            {
-                try
-                {
-                    _logger.Debug(() => string.Format(parameter, args));
-                }
-                catch (Exception ex)
-                {
-                    _logger.Debug(() => $"Failed to format string with arguments, ex: {ex.Message}");
-                }
-            };
-
-            Trace.TraceImplementation = (parameter, args) =>
-            {
-                try
-                {
-                    _logger.Info(string.Format(parameter, args));
-                }
-                catch (Exception ex)
-                {
-                    _logger.Info($"Failed to format string with arguments, ex: {ex.Message}");
-                }
-            };
-        }
-
-
-    }
-}
+//        public static void DefaultTraceInit()
+//        {
+//            Trace.TraceImplementation = (parameter, args) =>
+//            {
+//                try
+//                {
+//                    _logger.Debug(() => string.Format(parameter, args));
+//                }
+//                catch (Exception ex)
+//                {
+//                    _logger.Debug(() => $"Failed to format string with arguments, ex: {ex.Message}");
+//                }
+//            };
+//        }
+//    }
+//}
